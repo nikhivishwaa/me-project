b5fc6ef1711ac96d3bce02dc405e5778a8df28ed
Nikhil Vishwakarma <121393371+nikhivishwaa@users.noreply.github.com>
2024-06-23 16:55:53 +0530

    validations implemented on gui

--- Readme.md
+++ Readme.md
@@ -3,3 +3,39 @@
 cd .\meapp\
 python manage.py runserver
 ```
+
+##### cloud eunning commands
+
+```
+sudo su
+```
+```
+cd /var/www/Mechanical-Project
+```
+```
+git pull git@github.com:nikhivishwaa/me-project.git
+```
+```
+source prodenv/bin/activate
+cd meapp/
+pip install -r requirements.txt
+```
+```
+python manage.py makemigrations
+```
+```
+python manage.py migrate
+```
+```
+python manage.py collectstatic
+```
+```
+yes
+```
+```
+python manage.py runserver
+```
+
+```
+deactivate
+```
--- meapp/home/views.py
+++ meapp/home/views.py
@@ -3,20 +3,18 @@ from django.http import HttpResponse
 from django.contrib.auth.models import User
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth import authenticate, login, logout
-import json
-from . import validators
+from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from . import validators
+import json
 
+@login_required(login_url="/login")
 def home(request):
-    if not request.user.is_authenticated:
-        return redirect('login')
-
-    print(request.user.username)
     return render(request,'home.html', context={'walls': range(1,5)})
 
 def register(request):
     if request.method == 'POST':
-        email = request.POST.get('email', '')
+        email = request.POST.get('email', '').lower()
         password = request.POST.get('password', '')
         firstname = request.POST.get('firstname', '')
         lastname = request.POST.get('lastname', '')
@@ -24,15 +22,15 @@ def register(request):
         if email and password and firstname:
             if not validators.validate_email(email):
                 messages.error(request, "Invalid Email Address")
-                return render(request, 'login.html', context = {'page':'signup'})
+                return render(request, 'signup.html', context = {'page':'signup'})
             
             user = User.objects.filter(email=email)
             if user.exists():
                 messages.error(request, "User already exist")
-                return render(request, 'login.html', context = {'page':'signup'})
+                return render(request, 'signup.html', context = {'page':'signup'})
 
             else:
-                username = email.split('@')[0].replace('.','')
+                username = email.split('@')[0].replace('.','_')
                 newuser = User.objects.create_user(username = username, 
                                                     email = email, 
                                                     first_name = firstname.strip(), 
@@ -42,19 +40,19 @@ def register(request):
                 print('User created', newuser)
                 newuser.save()
                 messages.success(request, "Account Created. Now you can Login")
-                return render(request, 'login.html', context = {'page':'signup'})
+                return render(request, 'signup.html', context = {'page':'signup'})
         else:
             messages.error(request, "Please fill all the required fields")
-            return render(request, 'login.html', context = {'page':'signup'})
+            return render(request, 'signup.html', context = {'page':'signup'})
 
-    return render(request, 'login.html', context = {'page':'signup'}) 
+    return render(request, 'signup.html', context = {'page':'signup'}) 
 
 def logoutuser(request):
-    if request.user.is_authenticated:
-        logout(request)
-        messages.success(request, 'User has been logged out')
+    logout(request)
+    # messages.success(request, 'User has been logged out')
     return redirect('login')
 
+@login_required(login_url="/login")
 def profile(request):
     if request.user.is_authenticated:
         user = {
@@ -71,23 +69,28 @@ def userauth(request):
         return redirect('/')
 
     elif request.method == 'POST':
-        email = request.POST.get('email', '')
+        email = request.POST.get('email', '').lower()
         password = request.POST.get('password', '')
         if email and password:
-            username = email.split('@')[0].replace('.','')
-            user = authenticate(username=username, password=password)
-            # print('user', user)
-            if user is not None:
-                login(request, user)
-                return redirect('/')
+            try:
+                username = User.objects.get(email=email).username
+                print('username', username)
+                user = authenticate(username=username, password=password)
+                print('user', user)
+                if user is not None:
+                    login(request, user)
+                    return redirect('/')
 
-            else:
-                messages.error(request, "Invalid Credentials")
-                return render(request, 'login.html', context = {'page':'login'})
+                else:
+                    messages.error(request, "Invalid Credentials")
+                    return render(request, 'login.html')
                 
+            except Exception as e:
+                messages.error(request, "User not exist")
+                return render(request, 'login.html')
         else:
             messages.error(request, f"{'Password' if not password else 'Email'} is missing")
-            return render(request, 'login.html', context = {'page':'login'})
+            return render(request, 'login.html')
 
 
-    return render(request, 'login.html', context = {'page':'login'})
+    return render(request, 'login.html')
